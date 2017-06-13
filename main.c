4a991c3d65b098c4292abfc294a69b8c90875486
20161104612 <a20161104612@Mac28.local>
2017-06-13 09:53:11 +0800

    Date

--- main.c
+++ main.c
@@ -7,17 +7,22 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#define MAX 10
 
 int main()
 {
-    int a[10];
+    FILE *fw;
+    FILE *fp;
+    fw=fopen("/Users/a20161104612/Desktop/20161104612/sort/input.txt","r+");
+    fp=fopen("/Users/a20161104612/Desktop/20161104612/sort/output.txt","w");
+    int a[MAX];
     int i,j,t;
-    for(i=0;i<10;i++)
+    while(a[i]!='0')
     {
-        scanf("%d",&a[i]);
-    }
-    for(j=0;j<9;j++)
-    {
+       
+        for(j=0;j<9;j++)
+       {
         for(i=0;i<=8-j;i++)
         {
             if(a[i]>a[i+1])
@@ -30,8 +35,17 @@ int main()
     }
     for(i=0;i<10;i++)
     {
-        printf("%d ",a[i]);
+        printf("%d",a[i]);
+        fprintf(fp,"%d",a[i]);
     }
+    }
+    
+    
+    fclose(fw);
+    fclose(fp);
+    
+    
     printf("\n");
     return 0;
 }
+
